75eda92633cae2b58f1ea01b67c304755c70ab37
Mrbabet <wieliczkokamil@gmail.com>
2024-05-14 21:35:23 +0200

    .

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,24 +4,10 @@ import { useDispatch } from "react-redux";
 import { refreshUser } from "../redux/auth/operations";
 
 const Home = () => {
-  const dispatch = useDispatch();
-
-  const refreshAccessToken = () => {
-    dispatch(refreshUser())
-      .unwrap()
-      .then((response) => {
-        console.log("Access token refreshed:", response);
-      })
-      .catch((error) => {
-        console.error("Failed to refresh access token:", error);
-      });
-  };
-
   return (
     <>
       <div>Home</div>
       <Link to={"/reports"}>Reports</Link>
-      <button onClick={refreshAccessToken}>Refresh Access Token</button>
     </>
   );
 };
--- src/pages/Reports.jsx
+++ src/pages/Reports.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
+import { refreshUser } from "../redux/auth/operations";
 
 const Reports = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [dispatch]);
   return (
     <>
       <div>Reports</div>
